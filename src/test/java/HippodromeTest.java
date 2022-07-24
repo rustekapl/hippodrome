bdf9697906d9e2fa9863ae9308d82e75de59d3a1
rusta <rustekapl@gmail.com>
2022-07-24 16:20:33 +0200

    add HippodromeTest.java move

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -7,52 +7,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class HippodromeTest {
 
     @Test
-    void nullParameterShouldThrowIllegalArgumentException(){
-        assertThrows(IllegalArgumentException.class,()->new Hippodrome(null));
+    public void nullParameterShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
     }
 
     @Test
-    public
-    void nullParameterShouldThrowIllegalArgumentExceptionWithMessageHorsesCannotBeNull() {
+    public void nullParameterShouldThrowIllegalArgumentExceptionWithMessageHorsesCannotBeNull() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
         String expectedMessage = "Horses cannot be null.";
         String actualMessage = exception.getMessage();
-        assertEquals(expectedMessage,actualMessage);
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
-    void blinkListParametersShouldThrowIllegalArgumentException(){
-        assertThrows(IllegalArgumentException.class,()->new Hippodrome(new ArrayList<>()));
+    void blinkListParametersShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
     }
 
     @Test
-    public
-    void blinkListParametersShouldThrowIllegalArgumentExceptionWithMessageHorsesCannotBeEmpty() {
+    public void blinkListParametersShouldThrowIllegalArgumentExceptionWithMessageHorsesCannotBeEmpty() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
         String expectedMessage = "Horses cannot be empty.";
         String actualMessage = exception.getMessage();
-        assertEquals(expectedMessage,actualMessage);
+        assertEquals(expectedMessage, actualMessage);
     }
-
 
 
     @Test
     void checkGetListOfHorses() {
         List<Horse> horses = new ArrayList<>();
         for (int i = 0; i < 30; i++) {
-            horses.add(new Horse("Horse"+i,10,10));
+            horses.add(new Horse("Horse" + i, 10, 10));
         }
-        Hippodrome hippodrome=new Hippodrome(horses);
-        assertEquals(horses,hippodrome.getHorses());
+        Hippodrome hippodrome = new Hippodrome(horses);
+        assertEquals(horses, hippodrome.getHorses());
 
     }
 
     @Test
-    void move() {
+    void checkMoveForAllHorses() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horses.add(mock(Horse.class));
+        }
+
+        Hippodrome hippodrome = new Hippodrome(horses);
+        hippodrome.move();
+
+        for (Horse horse :
+                horses) {
+            verify(horse).move();
+
+        }
+
     }
 
     @Test
