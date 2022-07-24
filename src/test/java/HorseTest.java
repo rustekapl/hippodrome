0158cd3f545ee38e5b23b0ebbcb2df773ef783ca
rusta <rustekapl@gmail.com>
2022-07-24 14:40:33 +0200

    add constructor tests to HorseTest.java

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,50 +1,121 @@
-import static java.util.Objects.isNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class HorseTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    private final String name;
-    private final double speed;
-    private double distance;
+class HorseTest {
+    private Horse horse;
 
-    public HorseTest(String name, double speed, double distance) {
-        if (isNull(name)) {
-            throw new IllegalArgumentException("Name cannot be null.");
-        } else if (name.isBlank()) {
-            throw new IllegalArgumentException("Name cannot be blank.");
-        }
-        if (speed < 0) {
-            throw new IllegalArgumentException("Speed cannot be negative.");
-        }
-        if (distance < 0) {
-            throw new IllegalArgumentException("Distance cannot be negative.");
-        }
-
-        this.name = name;
-        this.speed = speed;
-        this.distance = distance;
+    @BeforeEach
+    void init() {
+        horse = new Horse("Horse", 100, 1000);
     }
 
-    public HorseTest(String name, double speed) {
-        this(name, speed, 0);
+    @AfterEach
+    void destroy() {
     }
 
-    public String getName() {
-        return name;
+    //constructor 1
+    @Test
+    public void nullFirstParameterShouldThrowIllegalArgumentException() {
+
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0));
     }
 
-    public double getSpeed() {
-        return speed;
+    //constructor 2
+    @Test
+    public void nulFirstParameterShouldThrowIllegalArgumentExceptionWithMessageNameCanntBeNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 0));
+        String expectedMessage = "Name cannot be null.";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage,actualMessage);
     }
 
-    public double getDistance() {
-        return distance;
+    //constructor 3
+    @ParameterizedTest
+    @CsvSource({
+            ",0",
+            "   ,0"
+    })
+    public void blankFirstParameterThrowIllegalArgumentException(String name,double speed) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name,speed));
     }
 
-    public void move() {
-        distance += speed * getRandomDouble(0.2, 0.9);
+
+    //constructor 4
+    @Test
+    public void blankFirstParameterThrowIllegalArgumentExceptionWithMessageNameCannotBeBlank() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("    ",0));
+        String expectedMessage = "Name cannot be blank.";
+        String actualMessage = exception.getMessage();
+        assertEquals(expectedMessage,actualMessage);
     }
 
-    public static double getRandomDouble(double min, double max) {
-        return (Math.random() * (max - min)) + min;
+    //constructor 5
+    @Test
+    public void negativeSecondParameterShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", -1));
+
     }
+
+    //constructor 6
+    @Test
+    public void negativeSecondParameterShouldThrowIllegalArgumentExceptionWithMessageSpedCannotBeNegative(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", -1));
+        String expectedMessage = "Speed cannot be negative.";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    //constructor 7
+    @Test
+    public void negativeThirdParameterShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", 0,-1));
+
+    }
+
+    //constructor 8
+    @Test
+    public void negativeThirdParameterShouldThrowIllegalArgumentExceptionWithMessageSpedCannotBeNegative(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Horse", 0,-1));
+        String expectedMessage = "Distance cannot be negative.";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+    @Test
+    void getNameWhichWasInParameter() {
+        String actualName = horse.getName();
+        assertEquals("Horse", actualName);
+
+    }
+
+    @Test
+    void getSpeedWhichWasInParameter() {
+        double actualSpeed = horse.getSpeed();
+        assertEquals(100, actualSpeed);
+    }
+
+    @Test
+    void getDistanceWhichWasInParameter() {
+        double actualDistance= horse.getDistance();
+        assertEquals(1000,actualDistance);
+    }
+
+    @Test
+    void getNullDistanceIfConstructorDoesnotContainsThirdParameter() {
+        double actualDistance= (new Horse("Horse",100)).getDistance();
+        assertEquals(0,actualDistance);
+    }
+
+    @Test
+    void move() {
+    }
+
+
 }
